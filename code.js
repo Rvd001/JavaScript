a7737d1b31fcb391ffa7c672728a6f8fd4a6d0df
Rvd001 <fagbamigbekehinde@gmail.com>
2020-10-23 18:19:50 -0500

    Toolbar in tableau.Viz

--- code.js
+++ code.js
@@ -4,20 +4,22 @@ function initViz(){
     // var url = "https://public.tableau.com/views/JavaScriptProjectWorkbook/USA";
     var url = "https://public.tableau.com/views/JavaScriptProjectWorkbook/Overview";
     //url of the dashboard on Tableau Public
-    var viz = new tableau.Viz(containerDiv, url);
-    //A new object created for an inbuilt object/function called tableau with a property called Viz
+
 
 
     //This is an object containing some properties and their default values being set
     var options = {
-        hidetabs : false,
+        hidetabs : true,
         onFirstInteractive: function ()
             {
                 workbook = viz.getWorkbook(); //get the current view and get the workbook object
                 activeSheet = workbook.getActiveSheet(); 
                 //get the active sheet from the workbook object of the view
             }
-        }
+        };
+
+    var viz = new tableau.Viz(containerDiv, url, options);
+    //A new object created for an inbuilt object/function called tableau with a property called Viz
 };
 
 function barChart(){
