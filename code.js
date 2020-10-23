b4a0ab212fa83fbd2ad649eb04deb8d68327bb99
Rvd001 <fagbamigbekehinde@gmail.com>
2020-10-23 18:01:52 -0500

    Activating Buttons

--- code.js
+++ code.js
@@ -1,7 +1,8 @@
 function initViz(){
     var containerDiv = document.getElementById('"vizContainer"');
     //a variable that holds the div container's ID in the body of the html
-    var url = "https://public.tableau.com/views/JavaScriptProjectWorkbook/USA";
+    // var url = "https://public.tableau.com/views/JavaScriptProjectWorkbook/USA";
+    var url = "https://public.tableau.com/views/JavaScriptProjectWorkbook/Overview";
     //url of the dashboard on Tableau Public
     var viz = new tableau.Viz(containerDiv, url);
     //A new object created for an inbuilt object/function called tableau with a property called Viz
@@ -11,7 +12,22 @@ function initViz(){
         hidetabs : true,
         onFirstInteractive: function ()
             {
-            console.log("Run this code when the viz has finished loading.")
+                workbook = viz.getWorkbook(); //get the current view and get the workbook object
+                activeSheet = workbook.getActiveSheet(); 
+                //get the active sheet from the workbook object of the view
             }
         }
-}
+};
+
+function barChart(){
+    workbook.activateSheetAsync("Bar Chart")
+};
+
+function bubbleChart(){
+    workbook.activateSheetAsync("Bubble Chart")
+};
+
+function boxAndWhisker(){
+    workbook.activateSheetAsync("Box and Whisker")
+};
+
